7461a1c047d99771a9b2b5f969379bc9f39a91a7
xVares <mustafa-zeybek@protonmail.com>
2023-07-01 04:16:08 +0200

    feat: new promotions, better buy method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import products
 import store
+import promotions
 
 
 def start(final_store):
@@ -95,15 +96,27 @@ def main():
 
     Note: The function relies on the `start()` function to retrieve user input.
     """
-    # old_main
-    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                    products.Product("Google Pixel 7", price=500, quantity=250, ),
-                    products.NonStockedProduct("Windows License", price=125)
-                    ]
-    best_buy = store.Store(product_list)
-    start(best_buy)
 
+
+# setup initial stock of inventory
+product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                products.Product("Google Pixel 7", price=500, quantity=250),
+                products.NonStockedProduct("Windows License", price=125),
+                products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
+                ]
+
+# Create promotion catalog
+second_half_price = promotions.SecondHalfPrice("Second Half price!")
+third_one_free = promotions.ThirdOneFree("Third One Free!")
+thirty_percent = promotions.PercentDiscount("30% off!", percent=30)
+
+# Add promotions to products
+product_list[0].set_promotion(second_half_price)
+product_list[1].set_promotion(third_one_free)
+product_list[3].set_promotion(thirty_percent)
+
+start(store.Store(product_list))
 
 if __name__ == '__main__':
     main()
--- products.py
+++ products.py
@@ -1,3 +1,5 @@
+import promotions
+
 """classes for products"""
 
 
@@ -35,13 +37,15 @@ class Product:
             price (int): The price of the product.
             quantity (int): The quantity of the product.
             active (bool): The activation status of the product.
+            promotion (obj): The Promotion obj of the product.
+            maximum ()
         """
         validate_product_init_params(name, price, quantity)
-
         self.name = name
         self.price = price
         self.quantity = quantity
         self.active = True
+        self.promotion = None
 
     def get_quantity(self) -> int:
         """Getter function for quantity. Returns the quantity (integer)."""
@@ -52,6 +56,14 @@ class Product:
         self.quantity = quantity
         if self.quantity <= 0:
             self.deactivate()
+
+    def get_promotion(self):
+        """Getter function for product promotion."""
+        return self.promotion
+
+    def set_promotion(self, promotion):
+        """Setter function for product promotion object."""
+        self.promotion = promotion
 
     def is_active(self) -> bool:
         """Getter function for active. Returns True if the product is active, otherwise False."""
@@ -66,41 +78,89 @@ class Product:
         self.active = False
 
     def show(self) -> str:
-        """Returns a string that represents the product."""
-        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+        """Returns a string that represents the product and corresponding promotions."""
+        if self.promotion is None:
+            return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}, Promotion: " \
+               f"{self.promotion.get_name()}"
 
     def buy(self, quantity: int) -> float:
         """
-        Buys a given quantity of the product. Returns the total price (float) of the purchase.
-        Updates the quantity of the product. Raises a specific exception in case of a problem.
-        """
-        try:
-            # filter non-stocked products -> purchase always successful
-            if self.quantity == 0:
-                print(f"You successfully purchased {quantity} units of"
-                      f" {self.name} for {self.price * quantity}$!")
-                return float(self.price * quantity)
+        Processes an order for the product with the given quantity.
 
-            # check stock of stocked products in store
-            elif self.quantity > 0 > self.quantity - quantity:
+        Args:
+            quantity (int): The quantity of the product to be ordered.
+
+        Returns:
+            float: The total price of the purchase.
+
+        Raises:
+            ValueError: If the order quantity exceeds the stock in the store or exceeds the
+                maximum limit for LimitedProduct.
+
+        Notes:
+            - For NonStockedProduct:
+                the purchase is always successful, and the function returns the total price of
+                the purchase without affecting the quantity.
+
+            - For LimitedProduct:
+                if the order quantity exceeds the maximum limit, a ValueError is raised.
+                Otherwise, the quantity is deducted from the available stock.
+
+            - For other products, the order quantity is deducted from the available stock.
+
+        """
+        # TODO:
+        #   - Add exception for apply_promotion() method for products with no promotion
+        #       • With workaround for | self.promotion = NoneType ?
+        #   - more readable buy method ?
+        #       • shorten / simplify / add helper function
+        try:
+            # check if order quantity exceeds stock in store
+            if not isinstance(self, NonStockedProduct) and self.quantity - quantity < 0:
                 raise ValueError(f"Order quantity for {self.name} is larger than current stock: "
                                  f"({self.quantity})")
+
+            # filter non-stocked products -> purchase always successful
+            elif isinstance(self, NonStockedProduct):
+                print(f"You successfully purchased {quantity} units of"
+                      f" {self.name} for {self.promotion.apply_promotion(self, quantity)}$!")
+                return float(self.promotion.apply_promotion(self, quantity))
+
+            # filter limited products
+            elif isinstance(self, LimitedProduct):
+                if quantity > self.maximum:
+                    raise ValueError(f"Order quantity for {self.name} exceeds the maximum "
+                                     f"limit: ({self.maximum})")
+                self.set_quantity(self.quantity - quantity)
+                print(
+                    f"You successfully purchased {quantity} units of {self.name} for"
+                    f" {self.promotion.apply_promotion(self, quantity)}$!")
+                return float(self.promotion.apply_promotion(self, quantity))
+
             else:
                 self.set_quantity(self.quantity - quantity)
                 print(f"You successfully purchased {quantity} units of"
-                      f" {self.name} for {self.price * quantity}$!")
-                return float(self.price * quantity)
+                      f" {self.name} for {self.promotion.apply_promotion(self, quantity)}$!")
+                return float(self.promotion.apply_promotion(self, quantity))
         except ValueError as error:
             print(f"Error while making order! {error}")
             return 0.0
 
 
 class NonStockedProduct(Product):
+    """Subclass for products that have no tracking for their stock"""
+
     def __init__(self, name: str, price: int):
         super().__init__(name, price, 0)
 
 
 class LimitedProduct(Product):
+    """
+    Subclass for products with a maximum limit on the quantity that can be bought  in a
+    single order.
+    """
+
     def __init__(self, name: str, price: int, quantity: int, maximum: int):
         super().__init__(name, price, quantity)
         self.maximum = maximum
